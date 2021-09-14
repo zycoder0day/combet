77347f90db6dd47df65d8266eadf79e1897bd068
mrcombet <67431127+mrcombet@users.noreply.github.com>
2021-09-15 02:45:32 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<link rel="icon" type="image/png" href="https://d.top4top.io/p_1973nl3q00.png"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXbEOb-eH3k8PGyE4FeISCORKsu0x-wh7Kuw&usqp=CAU">
+<meta property="og:title" content="Hacked By Mr.Combet">
+<meta property="og:description" content="One Hat Cyber Team"/>
+<title>HACKED BY MR.COMBER</title>
+<BODY ONCONTEXTMENU='return false;' ONKEYDOWN='return false;' ONMOUSEDOWN='return false;'>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Graduate&display=swap" rel="stylesheet">
+</head>
+<body BGCOLOR="black">
+<style type="text/css"> body{ background-color: black;background-size : cover;background-position: center;background-image: url("https://i.gifer.com/PD7X.gif");} div.content{ color: white; font-family: 'Oswald', sans-serif; font-size: 40px; text-align: center; } div.body{ color: white; font-family: 'Oswald', sans-serif; font-size: 15px; text-align: center; } div.xxx{ color: white; font-family: tahoma; font-size: 17px; text-align: center; } div.xxxx{ color: white; font-family:courier; font-size: 17px; text-align: center; } </style>
+<script> function play(){ var audio = document.getElementById('lagu'); audio.play(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } </script>
+<style>.blink {
+animation: blink-animation 1s steps(5, start) infinite;
+-webkit-animation: blink-animation 1s steps(5, start) infinite;
+}
+@keyframes blink-animation {
+to {
+visibility: hidden;
+}
+}
+@-webkit-keyframes blink-animation {
+to {
+visibility: hidden;
+}
+}
+</style>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+<table width="100%" height="100%">
+<td align="center"><center>
+<font face="Graduate" color="white" size="6">HACKED BY MR.COMBET<br>
+<img src="https://d.top4top.io/p_1973nl3q00.png" width="300" height="300"><br>
+<font face="Graduate" color="white" size="5">SORRY, IF I'VE HACKED YOUR WEBSITE.<br>BECAUSE, YOU WEBSITES NOT SECURE.<br>PLEASE IMPROVE YOUR WEBSITE.<br>SORRY ADMIN, WE ARE NOT CRIMINALS.<br>WE JUST TESTING THE SECURITY YOUR WEBSITE.<br>THANKS YOU !!<br><br>
+<font face="times" color="red">&#8226<font color="white"> Tu4n Private 404 <font color="red">&#8226<font color="white"> Pir4t3cyb3r <font color="red">&#8226<font color="white"> Drayso <font color="red">&#8226<font color="white"> Mr.Quixter <font color="red">&#8226<font color="white"> Mr.Pin666 <font color="red">&#8226<font color="white"> IbalGanz <font color="red">&#8226<font color="white"> Mr.Casper <font color="red">&#8226</b><p>
+<button class="lagu" onclick="play();liat();"><font face="graduate" size="4" color="" ><b>Play Music</b></font></button><audio id="lagu" src="https://f.top4top.io/m_1936neik80.mp3"></audio><font face="graduate" color="white">  &#8226 &#8226 &#8226 &#8226  </font><button class="lagu" onclick="window.location.href='https://www.facebook.com/105637844802806'"><font face="graduate" size="4" color="" ><b>Fans Page</b></font></button></center></html>
+</center>
+</html>
